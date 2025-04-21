c93d5409503c5713e4d64efa4d10b19b385ff033
Ebaad Azam <ebaadazam82881@gmail.com>
2025-04-21 22:15:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-800 text-2xl font-bold'>
+Welcome <FaBeer/>
+    </div>
+  )
+}
+
+export default App
